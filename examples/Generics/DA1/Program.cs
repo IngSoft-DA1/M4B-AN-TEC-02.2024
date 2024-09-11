a8fbdc47347b8ab9ad42a3437ac93939adf00615
marcos <marcosbuydid@gmail.com>
2024-09-11 20:34:46 -0300

    add generics example

---
+++ examples/Generics/DA1/Program.cs
@@ -0,0 +1,14 @@
+﻿using GenericClass;
+
+GenericList<int> intList = new GenericList<int>();
+intList.Add(10);
+intList.Add(20);
+int itemOne = intList.Get(0);
+
+GenericList<string> stringList = new GenericList<string>();
+stringList.Add("Hello");
+stringList.Add("World");
+string itemTwo = stringList.Get(1);
+
+Console.WriteLine(itemOne);
+Console.WriteLine(itemTwo);
---
+++ examples/Generics/Generics/GenericList.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericClass;
+
+public class GenericList<T>
+{
+    private List<T> list = new List<T>();
+
+    public void Add(T item)
+    {
+        list.Add(item);
+    }
+
+    public T Get(int index)
+    {
+        return list[index];
+    }
+    
+}
