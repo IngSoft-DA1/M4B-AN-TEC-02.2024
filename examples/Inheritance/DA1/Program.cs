4183955f0518866a2d01273112910212960fe904
marcos <marcosbuydid@gmail.com>
2024-09-11 18:35:16 -0300

    refactor inheritance example

---
+++ examples/Inheritance/DA1/Program.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+
+List<Vehicle> vehicles = new List<Vehicle>();
+
+Car car = new Car(4,"red");
+Van van = new Van(2,"green");
+
+vehicles.Add(car);
+vehicles.Add(van);
+
+foreach (var vehicle in vehicles)
+{
+    vehicle.TurnOn();
+}
